50b7c10fcc0af11f4c0a5fc6d16bdb3d7e836752
m.m.m.elhossin@gmail.com <m.elhossin@bridge-bfc.com>
2025-06-24 11:20:05 +0300

    Update

--- app/Http/Controllers/LeaveUsageController.php
+++ app/Http/Controllers/LeaveUsageController.php
@@ -45,7 +45,7 @@ class LeaveUsageController extends Controller
     {
         $request->validate([
             'start_date' => 'required|date',
-            'end_date' => 'required|date',
+            'end_date' => 'required|date|after_or_equal:start_date',
             'type' => 'required|in:normal,urgent,sick',
             'employee_id' => 'required|exists:employees,id',
         ]);
@@ -121,7 +121,7 @@ class LeaveUsageController extends Controller
 
         $request->validate([
             'start_date'  => 'required|date',
-            'end_date'    => 'required|date',
+            'end_date'    => 'required|date|after_or_equal:start_date',
             'type'        => 'required',
         ]);
 
